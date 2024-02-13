92a9f3085b4fecb29047ad53bad89d8d64beb6b1
DeckerSU <deckersu@protonmail.com>
2024-02-13 15:16:19 +0100

    build: More OpenMP / Multicore related things removal

--- zcutil/build-mac.sh
+++ zcutil/build-mac.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# seems clang on native darwin doesn't support -fopenmp compiler flag (MULTICORE=1 for libsnark)
-sed -i.old -e 's|\(CURVE=ALT_BN128[ \t]*MULTICORE=\)\([0-9]\{1,\}\)|\10|' ./depends/packages/libsnark.mk
 make -C ${PWD}/depends v=1 NO_PROTON=1 HOST=x86_64-apple-darwin -j$(nproc --all)
 ./autogen.sh
 CXXFLAGS="-g0 -O2" \
