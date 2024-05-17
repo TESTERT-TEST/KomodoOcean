1af19aa0e638a1ff992009ad86adb6fc1a832072
DeckerSU <deckersu@protonmail.com>
2024-05-17 22:08:20 +0200

    Cleanup: fix the ENABLE_VIRTUAL_TERMINAL_PROCESSING redifinition

--- src/metrics.cpp
+++ src/metrics.cpp
@@ -35,7 +35,7 @@
 #include <string>
 #ifdef _WIN32
 #include <io.h>
-#include <windows.h>
+#include <wincon.h>
 #else
 #include <sys/ioctl.h>
 #endif
@@ -448,8 +448,6 @@ int printInitMessage()
 }
 
 #ifdef WIN32
-#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
-
 bool enableVTMode()
 {
     // Set output mode to handle virtual terminal sequences
