02c58b984e61d798089fa853190de84ca09b643c
DeckerSU <deckersu@protonmail.com>
2024-11-09 03:51:10 +0100

    add build-mac-arm-cross.sh for arm64/aarch64 darwin

---
+++ zcutil/build-mac-arm-cross.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# don't forget to place SDK into project folder before run this script
+mkdir -p ${PWD}/depends/SDKs
+#tar -C ${PWD}/depends/SDKs -xf ${PWD}/Xcode-13.2.1-13C100-extracted-SDK-with-libcxx-headers.tar.gz
+
+[ -f MacOSX12.1.sdk.tar.xz ] || curl -L https://download.kmd.sh/depends-sources/MacOSX12.1.sdk.tar.xz -o MacOSX12.1.sdk.tar.xz
+mkdir -p ./depends/SDKs/Xcode-13.2.1-13C100-extracted-SDK-with-libcxx-headers
+tar --strip-components=1 -xf MacOSX12.1.sdk.tar.xz -C ./depends/SDKs/Xcode-13.2.1-13C100-extracted-SDK-with-libcxx-headers
+
+# make deps
+make -C ${PWD}/depends v=1 NO_PROTON=1 HOST=arm64-apple-darwin DARWIN_SDK_PATH=${PWD}/depends/SDKs/Xcode-12.1-12A7403-extracted-SDK-with-libcxx-headers -j$(nproc --all)
+
+# here we need bit modify config.site for darwin cross-compile case,
+# to fix env command path and paths to cctools (ar, ranlib, strip, nm, etc.),
+# but probably we don't need to use $(toolchain_path) variable in
+# depends/Makefile for $(host_prefix)/share/config.site target.
+
+CONFIG_SITE="$PWD/depends/arm64-apple-darwin/share/config.site"
+FIX_PATH=${PWD}/depends/arm64-apple-darwin/native/bin/
+if test -n "${FIX_PATH-}"; then
+	sed -i.old 's|'${FIX_PATH}'env|'$(command -v env)'|g' ${CONFIG_SITE} && \
+	sed -i.old 's|'${FIX_PATH}'/|/|g' ${CONFIG_SITE}
+fi
+
+./autogen.sh
+LDFLAGS="-Wl,-no_pie" \
+CXXFLAGS="-g0 -O2" \
+CONFIG_SITE="$PWD/depends/arm64-apple-darwin/share/config.site" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
+# make app
+make -j$(nproc --all) # V=1
