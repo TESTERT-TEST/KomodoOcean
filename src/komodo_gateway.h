a541201dd51b4bf7681669e7dedeec153fca1b29
DeckerSU <deckersu@protonmail.com>
2025-03-19 08:55:33 +0100

    change -ac_autosettle default = true
    - https://github.com/marmarachain/marmara/commit/457b8ad31b1f2c0c65fbd92aa638516b78229efb

--- src/komodo_gateway.h
+++ src/komodo_gateway.h
@@ -2730,7 +2730,7 @@ void komodo_createnodetransactions()
     CBlockIndex *pIndexTip = chainActive.LastTip();
     int32_t nHeight = pIndexTip ? pIndexTip->GetHeight() : 0;
 
-    if(ASSETCHAINS_MARMARA != 0 && GetBoolArg("-ac_autosettle", false))   
+    if(ASSETCHAINS_MARMARA != 0 && GetBoolArg("-ac_autosettle", true))   
     {
         MarmaraRunAutoSettlement(nHeight, minersTransactions);        // run Marmara autosettlement, returns settlement transactions
     }
