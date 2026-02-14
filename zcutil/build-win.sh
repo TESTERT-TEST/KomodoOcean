0b32972298d666f2e748789cfd2c99352cf51780
Miner113 <89903767+Miner113@users.noreply.github.com>
2026-02-15 03:10:54 +0700

    45

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -6,27 +6,44 @@ CC=x86_64-w64-mingw32-gcc-posix
 PREFIX="$(pwd)/depends/$HOST"
 
 set -eu -o pipefail
-
 set -x
+
 cd "$(dirname "$(readlink -f "$0")")/.."
 
-make "$@" -C ${PWD}/depends V=1 HOST=x86_64-w64-mingw32
-./autogen.sh
-CONFIG_SITE="$PWD/depends/x86_64-w64-mingw32/share/config.site" CXXFLAGS="-DCURL_STATICLIB -g0 -O2" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
 
-WD=$PWD
+make "$@" -C depends V=1 HOST=$HOST
 
-# Build RandomX
-cd src/crypto/randomx
-if [ -f "build/librandomx.a" ]; then
-    echo "RandomX already built"
+RANDOMX_DIR="src/crypto/randomx"
+RANDOMX_BUILD_DIR="$RANDOMX_DIR/build"
+RANDOMX_LIB="$RANDOMX_BUILD_DIR/librandomx.a"
+
+cd "$RANDOMX_DIR"
+if [ -f "$RANDOMX_LIB" ]; then
+    echo "RandomX: $RANDOMX_LIB"
 else
+    rm -rf build
     mkdir -p build && cd build
-    CC="${CC} -g" CXX="${CXX} -g" cmake -DARCH=native ..
+    CC="${CC}" CXX="${CXX}" cmake \
+        -DCMAKE_SYSTEM_NAME=Windows \
+        -DCMAKE_C_COMPILER="${CC}" \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
+        -DCMAKE_C_FLAGS="-static-libgcc" \
+        -DCMAKE_CXX_FLAGS="-static-libstdc++" \
+        -DARCH=native \
+        ..
     make
     cd ..
 fi
+cd "$OLDPWD"
 
-cd $WD
+export LDFLAGS="$LDFLAGS -L$PWD/$RANDOMX_BUILD_DIR"
+export LIBS="$LIBS -lrandomx"
 
-make "$@" # V=1
+ls -la "$PWD/$RANDOMX_LIB"
+
+./autogen.sh
+CONFIG_SITE="$PWD/depends/$HOST/share/config.site" \
+    CXXFLAGS="-DCURL_STATICLIB -g0 -O2" \
+    ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
+
+make "$@"
