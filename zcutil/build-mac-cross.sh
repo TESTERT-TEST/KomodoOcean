d270a1a6966afe2aaf8d427bcb434c7cfab383a4
Miner113 <89903767+Miner113@users.noreply.github.com>
2026-02-15 10:00:53 +0700

    5467

--- zcutil/build-mac-cross.sh
+++ zcutil/build-mac-cross.sh
@@ -3,31 +3,28 @@
 # don't forget to place SDK into project folder before run this script
 mkdir -p ${PWD}/depends/SDKs
 tar -C ${PWD}/depends/SDKs -xf ${PWD}/Xcode-13.2.1-13C100-extracted-SDK-with-libcxx-headers.tar.gz
-# make deps
+
+# Сборка depends
 make -C ${PWD}/depends v=1 NO_PROTON=1 HOST=x86_64-apple-darwin -j$(nproc --all)
 
-# here we need bit modify config.site for darwin cross-compile case,
-# to fix env command path and paths to cctools (ar, ranlib, strip, nm, etc.),
-# but probably we don't need to use $(toolchain_path) variable in
-# depends/Makefile for $(host_prefix)/share/config.site target.
-
+# Корректировка config.site (как у вас)
 CONFIG_SITE="$PWD/depends/x86_64-apple-darwin/share/config.site"
 FIX_PATH=${PWD}/depends/x86_64-apple-darwin/native/bin/
 if test -n "${FIX_PATH-}"; then
-	sed -i.old 's|'${FIX_PATH}'env|'$(command -v env)'|g' ${CONFIG_SITE} && \
+    sed -i.old 's|'${FIX_PATH}'env|'$(command -v env)'|g' ${CONFIG_SITE} && \
 	sed -i.old 's|'${FIX_PATH}'/|/|g' ${CONFIG_SITE}
 fi
 
-./autogen.sh
-LDFLAGS="-Wl,-no_pie" \
-CXXFLAGS="-g0 -O2" \
-CONFIG_SITE="$PWD/depends/x86_64-apple-darwin/share/config.site" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
-
-WD=$PWD
-
+# ------------------------------------------------------------
+# Кросс-сборка RandomX для macOS с использованием тулчейна depends
 RANDOMX_DIR="src/crypto/randomx"
 RANDOMX_BUILD_DIR="$RANDOMX_DIR/build"
 RANDOMX_LIB="$RANDOMX_BUILD_DIR/librandomx.a"
+
+# Определяем компиляторы из toolchain depends
+TOOLCHAIN_BIN="$PWD/depends/x86_64-apple-darwin/native/bin"
+export CC="$TOOLCHAIN_BIN/x86_64-apple-darwin-clang"
+export CXX="$TOOLCHAIN_BIN/x86_64-apple-darwin-clang++"
 
 cd "$RANDOMX_DIR"
 if [ -f "$RANDOMX_LIB" ]; then
@@ -35,8 +32,7 @@ if [ -f "$RANDOMX_LIB" ]; then
 else
     rm -rf build
     mkdir -p build && cd build
-    # Явно задаём компиляторы и целевую систему (для кросс‑компиляции с Linux)
-    CC="${CC}" CXX="${CXX}" cmake \
+    cmake \
         -DCMAKE_SYSTEM_NAME=Darwin \
         -DCMAKE_C_COMPILER="${CC}" \
         -DCMAKE_CXX_COMPILER="${CXX}" \
@@ -47,6 +43,12 @@ else
     make
     cd ..
 fi
-cd $WD
+cd "$OLDPWD"
 
-make -j$(nproc --all) # V=1
+./autogen.sh
+LDFLAGS="-Wl,-no_pie" \
+CXXFLAGS="-g0 -O2" \
+CONFIG_SITE="$PWD/depends/x86_64-apple-darwin/share/config.site" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
+
+# Финальная сборка
+make -j$(nproc --all)
